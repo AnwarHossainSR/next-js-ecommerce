9db845e044ef20904c3e2c912cc9ad0668dbc75e
AnwarHossainSR <mahedianwar@gmail.com>
2022-03-10 22:36:15 +0600

    Design register page

--- pages/register.js
+++ pages/register.js
@@ -1,7 +1,103 @@
-import React from "react";
+import Head from "next/head";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
-const register = () => {
-  return <div>register</div>;
+const Register = () => {
+  const initialState = { name: "", email: "", password: "", cf_password: "" };
+  const [userData, setUserData] = useState(initialState);
+  const { name, email, password, cf_password } = userData;
+
+  const router = useRouter();
+
+  const handleChangeInput = (e) => {
+    const { name, value } = e.target;
+    setUserData({ ...userData, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const errMsg = valid(name, email, password, cf_password);
+
+    const res = await postData("auth/register", userData);
+  };
+
+  return (
+    <div>
+      <Head>
+        <title>Register Page</title>
+      </Head>
+
+      <form
+        className="mx-auto my-4"
+        style={{ maxWidth: "500px" }}
+        onSubmit={handleSubmit}
+      >
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            name="name"
+            value={name}
+            onChange={handleChangeInput}
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="exampleInputEmail1">Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            name="email"
+            value={email}
+            onChange={handleChangeInput}
+          />
+          <small id="emailHelp" className="form-text text-muted">
+            We will never share your email with anyone else.
+          </small>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="exampleInputPassword1">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+            name="password"
+            value={password}
+            onChange={handleChangeInput}
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="exampleInputPassword2">Confirm Password</label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword2"
+            name="cf_password"
+            value={cf_password}
+            onChange={handleChangeInput}
+          />
+        </div>
+
+        <button type="submit" className="btn btn-dark w-100">
+          Register
+        </button>
+
+        <p className="my-2">
+          Already have an account?{" "}
+          <Link href="/signin">
+            <a style={{ color: "crimson" }}>Login Now</a>
+          </Link>
+        </p>
+      </form>
+    </div>
+  );
 };
 
-export default register;
+export default Register;
